3a395e203d2b8c9065ac80e41933367832af4eeb
nitin <nikkalra88@gmail.com>
2022-11-05 15:01:10 +0530

    feat: Added config

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const config = {
+	local: "https://FTL-Scoring.heynitin.repl.co",
+	production: "https://FTL-Scoring-backend.heynitin.repl.co",
+	serverURL: function(){return this.production},
+};
