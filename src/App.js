e214e3b876441334ef66ae0946856e340112e5ce
nitin <nikkalra88@gmail.com>
2022-11-05 11:31:20 +0530

    style: improved css

--- src/App.js
+++ src/App.js
@@ -20,74 +20,77 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <form onSubmit={(e) => submitHandler(e)}>
-        <input
-          className="textfield"
-          value={gameWeek}
-          onChange={(e) => setGameWeek(e.target.value)}
-          placeholder="Enter Game Week"
-        />
-        <input
-          className="textfield"
-          value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)}
-          placeholder="Enter player's Name"
-        />
-        <input
-          type="checkbox"
-          value={exact}
-          onChange={() => setExact((prev) => !prev)}
-          id="exact"
-        />
-        <label htmlFor="exact">Find Exact match</label>
-        <button>Get Score</button>
-      </form>
-      <div className="result">
-        {!isLoading && (
-          <>
-            {players.response.map((item, idx) => {
-              return (
-                <div key={idx}>
-                  <div>
-                    {item.player.first_name + " " + item.player.second_name}:{" "}
-                    {item.points.total_points -
-                      item.points.bonus -
-                      (item.points.minutes >= 60
-                        ? 2
-                        : item.points.minutes >= 1
-                        ? 1
-                        : 0)}
-                  </div>
-                </div>
-              );
-            })}
-            {players.notFound.map((item, idx) => (
-              <div key={idx}>{item}: Not Found</div>
-            ))}
-          </>
-        )}
-        {!isLoading && (players.response.length || players.notFound.length) ? (
-          <div>
-            Total:{" "}
-            {players.response.reduce(
-              (total, item) =>
-                total +
-                item.points.total_points -
-                item.points.bonus -
-                (item.points.minutes >= 60
-                  ? 2
-                  : item.points.minutes >= 1
-                  ? 1
-                  : 0),
-              0
-            )}
-          </div>
-        ) : (
-          <></>
-        )}
-        {isLoading && <div>Loading...</div>}
-      </div>
-    </div>
-  );
+		<div className="App">
+			<form onSubmit={(e) => submitHandler(e)}>
+				<input
+					className="textfield"
+					value={gameWeek}
+					onChange={(e) => setGameWeek(e.target.value)}
+					placeholder="Enter Game Week"
+				/>
+				<input
+					className="textfield"
+					value={playerName}
+					onChange={(e) => setPlayerName(e.target.value)}
+					placeholder="Enter player's Name"
+				/>
+				<input
+					type="checkbox"
+					value={exact}
+					onChange={() => setExact((prev) => !prev)}
+					id="exact"
+				/>
+				<label htmlFor="exact">Find Exact match</label>
+				<button>Get Score</button>
+			</form>
+			<div className="result">
+				{!isLoading && (
+					<>
+          <div className="bold">Found: {players.response.length}</div>
+						<ul>
+							{players.response.map((item, idx) => {
+								return (
+									<li key={idx + 1}>
+										{idx + 1}:{" "}
+										{item.player.first_name + " " + item.player.second_name}:{" "}
+										{item.points.total_points -
+											item.points.bonus -
+											(item.points.minutes >= 60
+												? 2
+												: item.points.minutes >= 1
+												? 1
+												: 0)}
+									</li>
+								);
+							})}
+						</ul>
+						<div className="bold">Not Found</div>
+						{players.notFound.map((item, idx) => (
+							<div key={idx}>{item}: Not Found</div>
+						))}
+					</>
+				)}
+				{!isLoading && (players.response.length || players.notFound.length) ? (
+					<div className="bold">
+						Total:{" "}
+						{players.response.reduce(
+							(total, item) =>
+								total +
+								item.points.total_points -
+								item.points.bonus -
+								(item.points.minutes >= 60
+									? 2
+									: item.points.minutes >= 1
+									? 1
+									: 0),
+							0
+						)}
+					</div>
+				) : (
+					<></>
+				)}
+				{isLoading && <div>Loading...</div>}
+			</div>
+		</div>
+	);
 }
